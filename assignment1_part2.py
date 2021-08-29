f0917f96366d5a141dc511a3e0a13aa837fef1e0
Connor Hanwick <chanwick@gmail.com>
2021-08-29 19:48:38 -0400

    Finished part 2

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,16 +1,34 @@
 #Part2
 
 class Book:
+    """
+    A class to represent a book
 
+    Attributes:
+        author (string): Name of the author
+        title (string): Title of the book
+        
+    """
     def __init__(self, author, title):
+        """
+        Constructs all the necessary attributes for the Book object.
+    
+        """
         self.author = author
         self.title = title
 
     def display(self):
-        print(self.title)
+        """
+    Returns:
+        (string) message containing the book's author and title
+        
+        """
+        print(self.title + ", written by " + self.author)
 
-book1 = Book("Will Chancellor", "7 Storeys")
-book2 = Book("Andy Warhol", "Popism")
+book1 = Book("John Steinbeck", "Of Mice and Men")
+book2 = Book("Harper Lee", "To Kill a Mockingbird")
 
-#book2.display()
-print(book2.author)
+
+book1.display()
+book2.display()
+# print(book2.author)
